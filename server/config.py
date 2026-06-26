209f726360f336ee3ca9fc8a79d83377c667049c
Aengus MacKenzie <aengus.2000@gmail.com>
2026-06-26 14:05:35 -0400

    Configure deployed database URL

--- server/config.py
+++ server/config.py
@@ -4,8 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+database_url = os.getenv("DATABASE_URL", "sqlite:///runereport.db")
+
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///runereport.db")
+    SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = os.getenv("SECRET_KEY")
